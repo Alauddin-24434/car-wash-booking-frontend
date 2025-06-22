4a44ea97f27918fa8bc46df178624140a649d1f1
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-06-23 01:32:04 +0600

    Clean main branch for fresh Next.js setup

--- src/routes/routes.tsx
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { userRoutes } from "./userRoutes";
-import { adminRoutes } from "./adminRoutes";
-
-export const router = createBrowserRouter([
-  ...userRoutes,
-  ...adminRoutes
-]);
